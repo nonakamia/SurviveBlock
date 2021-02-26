0cc6ee3212d5adfb3ffa7e2103ed9c7d8305b380
mia <kajiki>
2021-02-26 18:16:03 +0900

    画像

---
+++ tmx/tsx/obstacle.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="2019.12.11" name="obstacle" tilewidth="48" tileheight="48" tilecount="25" columns="5">
+ <image source="image/obstacle.png" width="240" height="240"/>
+</tileset>
